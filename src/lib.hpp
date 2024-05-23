0ac1bd34df93c8f650ceb7e785f7b922d695d141
Bolvic <victor.bolheme@gmail.com>
2024-05-23 23:08:08 +0200

    + | Adding intersect

--- src/lib.hpp
+++ src/lib.hpp
@@ -3,3 +3,10 @@
 #include <SFML/Graphics.hpp>
 #include "Player/Player.hpp"
 #include "Map/Map.hpp"
+
+bool	intersect(
+	sf::Vector2f &obj1_pos, 
+	sf::Vector2f &obj1_size, 
+	sf::Vector2f &obj2_pos, 
+	sf::Vector2f &obj2_size
+);
